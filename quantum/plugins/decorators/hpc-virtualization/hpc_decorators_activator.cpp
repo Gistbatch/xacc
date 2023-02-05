79ff9737ea10a6707ccc1f98a895762610c74acd
Philipp Seitz <philipp.seitz@tum.de>
2023-02-05 20:00:05 +0100

    Lint activator

--- quantum/plugins/decorators/hpc-virtualization/hpc_decorators_activator.cpp
+++ quantum/plugins/decorators/hpc-virtualization/hpc_decorators_activator.cpp
@@ -13,12 +13,12 @@
  *******************************************************************************/
 #include "hpc_async_decorator.hpp"
 #include "hpc_het_decorator.hpp"
+#include "hpc_scheduled_decorator.hpp"
 #include "hpc_virt_decorator.hpp"
 
 #include "cppmicroservices/BundleActivator.h"
 #include "cppmicroservices/BundleContext.h"
 #include "cppmicroservices/ServiceProperties.h"
-
 
 using namespace cppmicroservices;
 
@@ -26,35 +26,35 @@ namespace {
 
 /**
  */
-class US_ABI_LOCAL HPCDecoratorsActivator: public BundleActivator {
+class US_ABI_LOCAL HPCDecoratorsActivator : public BundleActivator {
 
 public:
+  HPCDecoratorsActivator() {}
 
-	HPCDecoratorsActivator() {
-	}
+  void Start(BundleContext context) {
+    auto c1 = std::make_shared<xacc::quantum::HPCAsyncDecorator>();
+    auto c2 = std::make_shared<xacc::quantum::HPCHetDecorator>();
+    auto c3 = std::make_shared<xacc::quantum::HPCScheduledDecorator>();
+    auto c4 = std::make_shared<xacc::quantum::HPCVirtDecorator>();
 
-	void Start(BundleContext context) {
-		auto c1 = std::make_shared<xacc::quantum::HPCAsyncDecorator>();
-		auto c2 = std::make_shared<xacc::quantum::HPCHetDecorator>();
-		auto c3 = std::make_shared<xacc::quantum::HPCVirtDecorator>();
+    context.RegisterService<xacc::AcceleratorDecorator>(c1);
+    context.RegisterService<xacc::Accelerator>(c1);
 
-		context.RegisterService<xacc::AcceleratorDecorator>(c1);
-        context.RegisterService<xacc::Accelerator>(c1);
+    context.RegisterService<xacc::AcceleratorDecorator>(c2);
+    context.RegisterService<xacc::Accelerator>(c2);
 
-        context.RegisterService<xacc::AcceleratorDecorator>(c2);
-        context.RegisterService<xacc::Accelerator>(c2);
+    context.RegisterService<xacc::AcceleratorDecorator>(c3);
+    context.RegisterService<xacc::Accelerator>(c3);
 
-		context.RegisterService<xacc::AcceleratorDecorator>(c3);
-        context.RegisterService<xacc::Accelerator>(c3);
-	}
+    context.RegisterService<xacc::AcceleratorDecorator>(c4);
+    context.RegisterService<xacc::Accelerator>(c4);
+  }
 
-	/**
-	 */
-	void Stop(BundleContext /*context*/) {
-	}
-
+  /**
+   */
+  void Stop(BundleContext /*context*/) {}
 };
 
-}
+} // namespace
 
 CPPMICROSERVICES_EXPORT_BUNDLE_ACTIVATOR(HPCDecoratorsActivator)
