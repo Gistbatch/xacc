969f16568b2c098458fa062b9fd12d173e061b67
Philipp Seitz <philipp.seitz@tum.de>
2023-02-06 09:58:34 +0100

    Clean up decorator impl

--- quantum/plugins/decorators/hpc-virtualization/hpc_scheduled_decorator.cpp
+++ quantum/plugins/decorators/hpc-virtualization/hpc_scheduled_decorator.cpp
@@ -21,12 +21,11 @@ namespace xacc {
 namespace quantum {
 
 void HPCScheduledDecorator::initialize(const HeterogeneousMap &params) {
-  xacc::info("Calling init worker");
   if (params.keyExists<std::string>("reference-handle")) {
     last = params.get<std::string>("reference-handle");
   }
   decoratedAccelerator->initialize(params);
-  xacc::info("launching worker");
+  xacc::info("Launching worker thread...");
   MyQueue my_jobs;
   jobs = std::make_shared<MyQueue>(my_jobs);
   my_thread = std::thread(&HPCScheduledDecorator::handle_jobs, this);
@@ -35,10 +34,8 @@ void HPCScheduledDecorator::initialize(const HeterogeneousMap &params) {
 
 void HPCScheduledDecorator::updateConfiguration(
     const HeterogeneousMap &config) {
-  xacc::info("Call update");
   if (config.keyExists<std::string>("reference-handle")) {
     last = config.get<std::string>("reference-handle");
-    xacc::info("Updated config to: " + last);
   }
   decoratedAccelerator->updateConfiguration(config);
 }
@@ -46,19 +43,17 @@ void HPCScheduledDecorator::updateConfiguration(
 void HPCScheduledDecorator::execute(
     std::shared_ptr<AcceleratorBuffer> buffer,
     const std::shared_ptr<CompositeInstruction> function) {
-  xacc::info("1Calling execute!");
   if (decoratedAccelerator) {
-    xacc::info("1Creating job");
-    auto task = std::make_shared<MyTask>([=]{
+    xacc::info("Creating new job ...");
+    auto task = std::make_shared<MyTask>([=] {
+      xacc::info("Calling Task for reference " + this->last + " ...");
       buffer->resetBuffer();
       this->decoratedAccelerator->execute(buffer, function);
-      buffer->print();
       return;
     });
-    xacc::info("1Submitting job at " + last);
     auto job = std::make_shared<std::pair<std::string, MyTaskPtr>>(last, task);
     jobs->push(job);
-    xacc::info("1Job submitted!");
+    xacc::info("Job submitted!");
   }
   return;
 }
@@ -66,18 +61,15 @@ void HPCScheduledDecorator::execute(
 void HPCScheduledDecorator::execute(
     std::shared_ptr<AcceleratorBuffer> buffer,
     const std::vector<std::shared_ptr<CompositeInstruction>> functions) {
-  xacc::info("Calling execute on multiple!");
   if (decoratedAccelerator) {
-    xacc::info("Creating job");
-    auto task = std::make_shared<MyTask>([=]{
-      xacc::info("Calling Task: " + this->last);
+    xacc::info("Creating new job ...");
+    auto task = std::make_shared<MyTask>([=] {
+      xacc::info("Calling Task for reference " + this->last + " ...");
       buffer->resetBuffer();
       this->decoratedAccelerator->execute(buffer, functions);
       xacc::info("Task called!");
-      buffer->print();
       return;
     });
-    xacc::info("Submitting job with ref " + last);
     auto job = std::make_shared<std::pair<std::string, MyTaskPtr>>(last, task);
     jobs->push(job);
     xacc::info("Job submitted!");
